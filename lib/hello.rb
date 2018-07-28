f2d80882c46e82bf2971e52118e8783255867de4
Eli Cho <eli.cho@gmail.com>
2018-07-28 04:38:44 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,9 +2,18 @@ def hello_t(array)
   i = 0
   
   while i < arrray.length
-  yield array[i]
-   i += 1
- end
+    yield array[i]
+    i += 1
+  end
+end
+
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 
 # call your method here!
